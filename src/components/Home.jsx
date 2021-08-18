b87a8de7086305dc4c71ff6b00ae274e8c05781e
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-08-18 16:53:58 +0200

    home terminée

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, {
-  Autoplay,
-  Navigation,
-  Pagination
-} from 'swiper';
+import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
 import './presentationBatiment.css';
-import photo1 from '../photos/photo1.jpg';
-import photo2 from '../photos/photo2.jpg';
-import photo3 from '../photos/photo3.jpg';
-import photo4 from '../photos/photo4.jpg';
-import photo5 from '../photos/photo5.jpg';
+import photo1 from '../photos/photo1.jpeg';
+import photo2 from '../photos/photo2.jpeg';
+import photo3 from '../photos/photo3.jpeg';
 import actu1 from '../actu/actu1.png';
 import actu2 from '../actu/actu2.jpeg';
 import actu3 from '../actu/actu3.jpeg';
 import './home.css';
-import separation_horizontale from '../images/separation_horizontale.png'
+import separation_horizontale from '../images/separation_horizontale.png';
 
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
@@ -25,34 +19,42 @@ function Home() {
     <div className='Home'>
       <div className='corps'>
         <aside>
-            <div className='actualites'>
-                <div className='article1'>
-                    <h6>La presse en parle</h6>
-                    <img src={actu1} className='actu3' alt='' />
-                    <p>
-                    Après avoir réalisé les aménagements du stade de football de
-                    Kransnodar en RUSSIE, le hasard à...
-                    </p>
-                </div>
-                <img src={separation_horizontale} className="separation_horizontale" alt="separation_horizontale"/>
-                <div className='article2'>
-                    <h6>Nouveau site internet</h6>
-                    <img src={actu2} className='actu2' alt='' />
-                    <p>
-                    Nous sommes très heureux de vous accueillir sur le nouveau site de
-                    notre activité voirie ...
-                    </p>
-                </div>
-                <img src={separation_horizontale} className="separation_horizontale" alt="separation_horizontale"/>
-                <div className='article1'>
-                    <h6>Nouveau polissoir à plat</h6>
-                    <img src={actu3} className='actu3' alt='' />
-                    <p>
-                    Un nouveau polissoir à plat a été installé au premier semestre
-                    2017. Cet outil nous...
-                    </p>
-                </div>
+          <div className='actualites'>
+            <div className='article1'>
+              <h6>La presse en parle</h6>
+              <img src={actu1} className='actu3' alt='' />
+              <p>
+                Après avoir réalisé les aménagements du stade de football de
+                Kransnodar en RUSSIE, le hasard à...
+              </p>
             </div>
+            <img
+              src={separation_horizontale}
+              className='separation_horizontale'
+              alt='separation_horizontale'
+            />
+            <div className='article2'>
+              <h6>Nouveau site internet</h6>
+              <img src={actu2} className='actu2' alt='' />
+              <p>
+                Nous sommes très heureux de vous accueillir sur le nouveau site
+                de notre activité voirie ...
+              </p>
+            </div>
+            <img
+              src={separation_horizontale}
+              className='separation_horizontale'
+              alt='separation_horizontale'
+            />
+            <div className='article1'>
+              <h6>Nouveau polissoir à plat</h6>
+              <img src={actu3} className='actu3' alt='' />
+              <p>
+                Un nouveau polissoir à plat a été installé au premier semestre
+                2017. Cet outil nous...
+              </p>
+            </div>
+          </div>
         </aside>
         <Swiper
           className='allCarousel'
@@ -67,19 +69,27 @@ function Home() {
           onSlideChange={() => console.log('slide change')}
         >
           <SwiperSlide className='photo'>
-            <img src={photo1} alt='' />
+            <div className='container'>
+              <img src={photo1} alt='' />
+              <div className='text'>
+                Monument commémoratif en mémoire des victimes de la SHOA pour la
+                ville de Luxembourg.
+              </div>
+            </div>
           </SwiperSlide>
           <SwiperSlide className='photo'>
-            <img src={photo2} alt='' />
+            <div className='container'>
+              <img src={photo2} alt='' />
+              <div className='text'>
+                Abords du Stade de foot du FC Krasnodar en RUSSIE
+              </div>
+            </div>
           </SwiperSlide>
           <SwiperSlide className='photo'>
-            <img src={photo3} alt='' />
-          </SwiperSlide>
-          <SwiperSlide className='photo'>
-            <img src={photo4} alt='' />
-          </SwiperSlide>
-          <SwiperSlide className='photo'>
-            <img src={photo5} alt='' />
+            <div className='container'>
+              <img src={photo3} alt='' />
+              <div className='text'>Dallage Hôtel de ville Vincennes</div>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
