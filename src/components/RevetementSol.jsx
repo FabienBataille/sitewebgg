4ac9083a216e4178245364dd53f0a0265c7c65e6
Fabien Bataille <fabienbataille.dev@gmail.com>
2021-08-24 15:06:09 +0200

    revetement sol

--- src/components/RevetementSol.jsx
+++ src/components/RevetementSol.jsx
@@ -8,59 +8,35 @@ import SwiperCore, {
   EffectFade,
 } from 'swiper';
 import 'swiper/swiper-bundle.css';
-import './escaliers.css';
-import escaliers1 from '../escaliers_photos/Escaliers1.jpg';
-import escaliers2 from '../escaliers_photos/Escaliers2.jpg';
-import escaliers3 from '../escaliers_photos/Escaliers3.jpg';
-import escaliers4 from '../escaliers_photos/Escaliers4.jpg';
-import escaliers5 from '../escaliers_photos/Escaliers5.jpg';
-import escaliers6 from '../escaliers_photos/Escaliers6.jpg';
-import escaliers7 from '../escaliers_photos/Escaliers7.jpg';
-import escaliers8 from '../escaliers_photos/Escaliers8.jpg';
+import './revetementSol.css';
+import sol1 from '../sols/sol1.jpeg';
+import sol2 from '../sols/sol2.jpeg';
 
 SwiperCore.use([Navigation, Pagination, Autoplay, Thumbs, EffectFade]);
 
-function Escaliers() {
+function RevetementSol() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
-    <div className='Escaliers'>
+    <div className='RevetementSol'>
       <Swiper
-        className='a'
+        className='a-sol'
         onSwiper={setThumbsSwiper}
         spaceBetween={30}
-        slidesPerView={8}
+        slidesPerView={2}
         freeMode={true}
         watchSlidesVisibility={true}
         watchSlidesProgress={true}
       >
-        <SwiperSlide className='vignette'>
-          <img src={escaliers1} alt='' />
+        <SwiperSlide className='vignette-sol'>
+          <img src={sol1} alt='' />
         </SwiperSlide>
-        <SwiperSlide className='vignette'>
-          <img src={escaliers2} alt='' />
-        </SwiperSlide>
-        <SwiperSlide className='vignette'>
-          <img src={escaliers3} alt='' />
-        </SwiperSlide>
-        <SwiperSlide className='vignette'>
-          <img src={escaliers4} alt='' />
-        </SwiperSlide>
-        <SwiperSlide className='vignette'>
-          <img src={escaliers5} alt='' />
-        </SwiperSlide>
-        <SwiperSlide className='vignette'>
-          <img src={escaliers6} alt='' />
-        </SwiperSlide>
-        <SwiperSlide className='vignette'>
-          <img src={escaliers7} alt='' />
-        </SwiperSlide>
-        <SwiperSlide className='vignette'>
-          <img src={escaliers8} alt='' />
+        <SwiperSlide className='vignette-sol'>
+          <img src={sol2} alt='' />
         </SwiperSlide>
       </Swiper>
       <Swiper
-        className='allCarousel-escaliers'
+        className='allCarousel-sol'
         effect='fade'
         spaceBetween={50}
         slidesPerview={3}
@@ -72,83 +48,36 @@ function Escaliers() {
         onSwiper={() => console.log('Swiper')}
         onSlideChange={() => console.log('slide change')}
       >
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers1} alt='' />
-            <div className='title-escaliers'>
-              Escalier Intérieur mémorial Colville sur mer
+        <SwiperSlide className='photo-sol'>
+          <div className='container-sol'>
+            <img src={sol1} alt='' />
+            <div className='title-sol'>Hall d'entrée Champs Libres Rennes</div>
+            <div className='text-sol'>Type granit : Lanhelin adouci</div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className='photo-sol'>
+          <div className='container-sol'>
+            <img src={sol2} alt='' />
+            <div className='title-sol'>Revêtement 50 avenue Montaigne</div>
+            <div className='text-sol'>
+              Type granit : Lanhelin | Giallo Vénéziano poli, adouci, bouchardé
             </div>
-            <div className='text-escaliers'>Type granit : Cachemir White flammé</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers2} alt='' />
-            <div className='title-escaliers'>Emmarchement Pays-Bas</div>
-            <div className='text-escaliers'>Type granit : Lanhelin flammé</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers3} alt='' />
-            <div className='title-escaliers'>Escalier métro Rennes République</div>
-            <div className='text-escaliers'>Type granit : Louvigné flammé</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers4} alt='' />
-            <div className='title-escaliers'>La Défense Paris</div>
-            <div className='text-escaliers'>Type granit : Lanhelin flammé</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers5} alt='' />
-            <div className='title-escaliers'>
-              Escalier extérieur avenue de France PARIS
-            </div>
-            <div className='text-escaliers'>Type granit : Jaune Aurore grenaillé</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers6} alt='' />
-            <div className='title-escaliers'>Escalier Opéra Bastille Paris</div>
-            <div className='text-escaliers'>Type granit : Lanhelin flammé</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers7} alt='' />
-            <div className='title-escaliers'>
-              Escalier/ bancs Mémorial Colleville Sur Mer
-            </div>
-            <div className='text-escaliers'>Type granit : Cachemir White flammé</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className='photo-escaliers'>
-          <div className='container-escaliers'>
-            <img src={escaliers8} alt='' />
-            <div className='title-escaliers'>
-              Escalier extérieur avenue de France PARIS
-            </div>
-            <div className='text-escaliers'>Type granit : Jaune Aurore grenaillé</div>
           </div>
         </SwiperSlide>
       </Swiper>
-      <div className='text-fixe-escaliers'>
+      <div className='text-fixe-sol'>
         <p>
-          La réalisation d’un escalier est le point d’orgue d’un projet. Il
-          valorise l’espace,<br /> le choix des matériaux est ainsi un point
-          essentiel.<br />
-          Pour que cet escalier soit unique et mémorable, nos experts sont à
-          votre disposition pour vous conseiller (type du granits, texture,
-          accessibilité, normes).
+          Le choix d’un dallage vous offre l’opportunité de créer une atmosphère
+          qui sera l’âme de votre projet. Les différentes finitions que nous
+          proposons (poli, adouci, flammé, grenaillé…) permettent de répondre à
+          tous les projets de revêtements intérieurs. Investir dans le granit
+          est un gage de pérennité assuré. Que votre aménagement intérieur soit
+          traditionnel ou contemporain, le granit est le matériau noble qui
+          saura vous ravir.
         </p>
       </div>
     </div>
   );
 }
 
-export default Escaliers;
+export default RevetementSol;
--- src/components/RevetementV.jsx
+++ src/components/RevetementV.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {
+  Autoplay,
+  Navigation,
+  Pagination,
+  Thumbs,
+  EffectFade,
+} from 'swiper';
+import 'swiper/swiper-bundle.css';
+import './revetementV.css';
+import sol1 from '../sols/sol1.jpeg';
+import sol2 from '../sols/sol2.jpeg';
+
+SwiperCore.use([Navigation, Pagination, Autoplay, Thumbs, EffectFade]);
 
 function RevetementV() {
-    return (
-        <div>
-            <h1>REVÊTEMENTS VERTICAUX</h1>
-        </div>
-    )
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  return (
+    <div className='RevetementV'>
+      <Swiper
+        className='a-verticaux'
+        onSwiper={setThumbsSwiper}
+        spaceBetween={30}
+        slidesPerView={2}
+        freeMode={true}
+        watchSlidesVisibility={true}
+        watchSlidesProgress={true}
+      >
+        <SwiperSlide className='vignette-sol'>
+          <img src={sol1} alt='' />
+        </SwiperSlide>
+        <SwiperSlide className='vignette-sol'>
+          <img src={sol2} alt='' />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        className='allCarousel-sol'
+        effect='fade'
+        spaceBetween={50}
+        slidesPerview={3}
+        autoplay={true}
+        /*navigation*/
+        /*pagination={{ clickable: true }}*/
+        thumbs={{ swiper: thumbsSwiper }}
+        /*scrollbar={{ draggable: true }}*/
+        onSwiper={() => console.log('Swiper')}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide className='photo-sol'>
+          <div className='container-sol'>
+            <img src={sol1} alt='' />
+            <div className='title-sol'>Hall d'entrée Champs Libres Rennes</div>
+            <div className='text-sol'>Type granit : Lanhelin adouci</div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className='photo-sol'>
+          <div className='container-sol'>
+            <img src={sol2} alt='' />
+            <div className='title-sol'>Revêtement 50 avenue Montaigne</div>
+            <div className='text-sol'>
+              Type granit : Lanhelin | Giallo Vénéziano poli, adouci, bouchardé
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div className='text-fixe-sol'>
+        <p>
+          Le choix d’un dallage vous offre l’opportunité de créer une atmosphère
+          qui sera l’âme de votre projet. Les différentes finitions que nous
+          proposons (poli, adouci, flammé, grenaillé…) permettent de répondre à
+          tous les projets de revêtements intérieurs. Investir dans le granit
+          est un gage de pérennité assuré. Que votre aménagement intérieur soit
+          traditionnel ou contemporain, le granit est le matériau noble qui
+          saura vous ravir.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default RevetementV
+export default RevetementV;
