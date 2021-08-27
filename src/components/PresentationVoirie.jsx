336dab971d24ab44bdeaa2320ae2a4485dd78188
FabienBataille <fabienbataille.dev@gmail.com>
2021-08-27 16:54:02 +0200

    presentation voirie ok

--- src/components/PresentationVoirie.jsx
+++ src/components/PresentationVoirie.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import Slideshow from './Slide'
+import presentation_voirie from '../images/presentation-voirie.jpeg'
+import './presentationVoirie.css'
 
 function PresentationVoirie() {
-    return (
-        <div>
-            <h1>PRÉSENTATION</h1>
-            <Slideshow />
-        </div>
-    )
+  return (
+    <div className="PresentationVoirie">
+      <h1>VOIRIE</h1>
+      <hr />
+      <div className="photo-text-batiment">
+        <img src={presentation_voirie} alt=""/>
+        <p>Leader dans la fabrication bretonne en granit, la Générale du Granit propose, depuis les années 1980, de répondre sur mesure aux projets urbains des municipalités.
+Elle vous offre une large gamme de produits en granit destinée aux aménagements urbains.Elle s'étend des dalles et pavés aux bordures en passant par les bornes.
+Grâce à nos savoir-faire et nos outils, nous concevons et mettons en œuvre vos projets de dallage et de pavage.
+Pour chaque projet, nous apportons des solutions à vos demandes, et ce, dans un souci de qualité et de durabilité.</p>
+      </div>
+    </div>
+  )
 }
 
 export default PresentationVoirie
